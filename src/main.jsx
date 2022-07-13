b9f402126619ea524c19e3509c34853362b3690f
KenjiAlex <sashakonocalov13@gmail.com>
2022-07-14 02:19:01 +0300

    app is totalydone, added a local storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import { store } from './redux/index'
+import store, { persistor } from './redux/index'
 import './index.scss'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 )
